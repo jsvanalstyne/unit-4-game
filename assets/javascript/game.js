b846f39c241d9bfd1466aaee60061985bbf235a3
Jerrica VanAlstyne <jsnow07@yahoo.com>
2019-09-27 22:06:00 -0400

    created necessary files for game

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+// -----Crystal collector PseudoCode----
+// Create html and css stylings for game based on demo
+// Need to randomly generate numbers for goal (math.floor) in an array
+// Display goal number in a div 
+// Create buttons for each crystal and assign it the image
+// Need to randomly generate numbers for crystals (4 different crystals)(math.floor)
+// Create on click functions for each crystal button
+// Create a way to add the crystals together after user selects crystal (add numbers to an array and sum the number?)
+// When user equals the goal number and the sum of the array --> winner, score goes up and game resets
+// Have current score display in a different div
+// If user exceeds goal numnber, loser and reset game
+// Create reset function that empties the arrays (score, goal and current sum)
