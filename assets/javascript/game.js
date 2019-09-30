9113577aa4477944b082616c771a5b4268c5c3e1
Jerrica VanAlstyne <jsnow07@yahoo.com>
2019-09-30 09:17:26 -0400

    added random numbers for winning number and crystals. Still working on counter

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,36 +1,48 @@
+{/* <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> */}
+
+
+
+
 // -----Crystal collector PseudoCode----
-// Create html and css stylings for game based on demo
-// Create variables for win, loss, random winning number, random crystal numbers (3)
+// Create html and css stylings for game based on demo **DONE
+// Create variables for win, loss, random winning number, counter, random crystal numbers (3)
+// $(document).ready(function() {
+var win = 0;
+var loss = 0;
+var counter = 0; 
 // Need to randomly generate numbers for goal (math.floor) in an array between 19 - 120.
-// Display goal number in a div 
+var winningRandom= Math.floor(Math.random()*102) + 19;
+console.log(winningRandom);  
 // Create buttons for each crystal and assign it the image between 1 - 12
 // Need to randomly generate numbers for crystals (4 different crystals)(math.floor)
+var silverCrystal = Math.floor(Math.random()*12) + 1;
+console.log(silverCrystal);
+var greenCrystal = Math.floor(Math.random()*12) + 1;
+console.log(greenCrystal);
+var redCrystal = Math.floor(Math.random()*12) + 1;
+console.log(redCrystal);
+var blueCrystal = Math.floor(Math.random()*12) + 1;
+console.log(blueCrystal);
+// Display goal number in a div 
+// $("#winningNumber").html(winningRandom);
 // Create on click functions for each crystal button
+$("#greenCrystal").on("click", function(){
+counter += greenCrystal;
+});
+$("#silverCrystal").on("click", function(){
+counter += silverCrystal;
+});
+$("#redCrystal").on("click", function(){
+counter += redCrystal;
+});
+$("#blueCrystal").on("click", function(){
+counter += blueCrystal;
+});
+
 // Create a way to add the crystals together after user selects crystal (add numbers to an array and sum the number?)
 // When user equals the goal number and the sum of the array --> winner, score goes up and game resets
 // Have current score display in a different div
 // If user exceeds goal numnber, loser and reset game
 // Create reset function that empties the arrays (score, goal and current sum)
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script type="text/javascript"> </script>
 
-    // For each iteration, we will create an imageCrystal
-    var silverCrystal = $("<img>");
 
-    // First each crystal will be given the class ".crystal-image".
-    // This will allow the CSS to take effect.
-    silverCrystal.addClass("crystal-image");
-
-    // Each imageCrystal will be given a src link to the crystal image
-    silverCrystal.attr("src", "http://clipartmag.com/images/crystals-clipart-26.png");
-
-    // Each imageCrystal will be given a data attribute called data-crystalValue.
-    // This data attribute will be set equal to the array value.
-    // silverCrystal.attr("data-crystalvalue", numberOptions[i]);
-
-    // Lastly, each crystal image (with all it classes and attributes) will get added to the page.
-    $("#crystals").append(silverCrystal);
-
-  // This time, our click event applies to every single crystal on the page. Not just one.
-//   $(".crystal-image").on("click", function() {
-     
